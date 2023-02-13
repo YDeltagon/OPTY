dde617d99decac9109c1bf1519d3969ce94d88a9
YDeltagon <git@YDeltagon.fr>
2023-02-13 13:28:42 +0100

    fix

--- resources/Shortcut_AutoOptiShutdown.ps1
+++ resources/Shortcut_AutoOptiShutdown.ps1
@@ -1,16 +1,12 @@
-# Create a new shortcut object for admin mode
+# 2023-01-27 - standalone :          Standalone
+
+# Create a new shortcut object for user mode
 $WshShell = New-Object -ComObject WScript.Shell
-$Shortcut = $WshShell.CreateShortcut("C:\Users\Public\Desktop\AutoOptiShutdown (Admin).lnk")
+$Shortcut = $WshShell.CreateShortcut("C:\Users\Public\Desktop\AutoOptiShutdown (admin).lnk")
 
 # Set the shortcut target path and working directory
-$Shortcut.TargetPath = "C:\Windows\System32\cmd.exe"
-$Shortcut.Arguments = "/c start """" /D ""C:\OPTY_by-YannD"" /B ""AutoOptiShutdown.bat"""
+$Shortcut.TargetPath = "C:\OPTY_by-YannD\AutoOptiShutdown.bat"
 $Shortcut.WorkingDirectory = "C:\OPTY_by-YannD\"
-
-# Set the shortcut to run as admin
-$Shortcut.Hotkey = "CTRL+SHIFT+ALT+A"
-$Shortcut.IconLocation = "C:\Windows\System32\cmd.exe, 0"
-$Shortcut.Run = "powershell.exe"
 
 # Save the shortcut
 $Shortcut.Save()
